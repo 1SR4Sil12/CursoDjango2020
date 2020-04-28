5426663454efec021b6e353b16976f8e8b0ebcee
Israel Silva <escorpion45678@gmail.com>
2020-04-28 19:15:50 +0200

    commit 31
    Seguimos con la práctica. Registration Redux finalizado

--- pd110/src/pd110/settings.py
+++ pd110/src/pd110/settings.py
@@ -150,6 +150,8 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_env", "media_root")
 
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+SITE_ID = 1
+
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 
